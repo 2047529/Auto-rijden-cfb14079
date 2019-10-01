3750a3dba50c15f2704d6d922d2a6a001479dcfe
Arjen Bos <2047529@talnet.nl>
2019-10-01 09:10:49 +0200

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,16 @@
+<?php
+
+$vraag = "Hoe oud ben je?\n";
+echo $vraag;
+$leeftijd = readline();
+
+if ($leeftijd >= 16.5)
+echo "Je bent oud genoeg, je mag beginnen";
+
+else 
+echo "Sorry, je bent nog te jong";
+
+
+
+
+?>
